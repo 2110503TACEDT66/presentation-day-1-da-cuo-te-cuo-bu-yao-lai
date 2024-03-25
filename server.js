d8c09b5dff9e1e143874dc6b3dff7904dd5aad00
mingssii <6633267521@student.chula.ac.th>
2024-03-25 18:09:44 +0700

    add vercel

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db.js');
-const cookieParser= require('cookie-parser');
-const mongoSanitize=require('express-mongo-sanitize')
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
-const {xss}= require('express-xss-sanitizer')
-const rateLimit=require('express-rate-limit')
-const hpp=require('hpp');
-const cors=require('cors')
+const { xss } = require('express-xss-sanitizer')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp');
+const cors = require('cors')
 
 
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 
 connectDB();
 
-const restaurants= require('./routes/restaurants.js');
-const auth= require('./routes/auth.js');
-const reservations= require('./routes/reservations.js')
+const restaurants = require('./routes/restaurants.js');
+const auth = require('./routes/auth.js');
+const reservations = require('./routes/reservations.js')
 const menus = require('./routes/menus.js');
 
-const limiter=rateLimit({
-    windowsMs:10*60*1000,
-    max:100
+const limiter = rateLimit({
+    windowsMs: 10 * 60 * 1000,
+    max: 100
 })
 
 
@@ -37,18 +37,18 @@ app.use(xss());
 app.use(limiter);
 app.use(hpp());
 app.use(cors());
-app.use('/api/v1/restaurants',restaurants);
-app.use('/api/v1/auth',auth);
-app.use('/api/v1/reservations',reservations);
-app.use('/api/v1/menus',menus);
+app.use('/api/v1/restaurants', restaurants);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/reservations', reservations);
+app.use('/api/v1/menus', menus);
 
 
 
-const PORT = process.env.PORT|| 5000;
-const server=app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV,' mode on port ',PORT));
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', process.env.HOST, PORT));
 
-process.on('unhandledRejection',(err,promise)=>{
+process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`);
 
-    server.close(()=>process.exit(1));
+    server.close(() => process.exit(1));
 });
