0ae36b9ce26849c0cc2acfdf80460b7dec8214d1
mingssii <6633267521@student.chula.ac.th>
2024-03-26 10:36:34 +0700

    remove protect menus

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,25 +1,25 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 
-const ReservationSchema=new mongoose.Schema({
-    revDate:{
-        type:Date,
-        required:true
+const ReservationSchema = new mongoose.Schema({
+    revDate: {
+        type: Date,
+        required: true
     },
-    user:{
-        type:mongoose.Schema.ObjectId,
-        required:true
+    user: {
+        type: mongoose.Schema.ObjectId,
+        required: true
     },
-    restaurant:{
-        type:mongoose.Schema.ObjectId,
-        ref:'Restaurant',
-        required:true
+    restaurant: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Restaurant',
+        required: true
     },
-    createdAt:{
-        type:Date,
-        default:Date.now
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
 
 
 
-module.exports=mongoose.model('Reservation',ReservationSchema)
+module.exports = mongoose.model('Reservation', ReservationSchema)
--- routes/menus.js
+++ routes/menus.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const {getMenus,getMenu,addMenu,updateMenu,deleteMenu}= require('../controllers/menus')
+const { getMenus, getMenu, addMenu, updateMenu, deleteMenu } = require('../controllers/menus')
 
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 
-const {protect,authorize}= require('../middleware/auth')
+const { protect, authorize } = require('../middleware/auth')
 
-router.route('/').get(protect,getMenus).post(protect,authorize('admin'),addMenu);
-router.route('/:id').get(protect,getMenu).put(protect,authorize('admin'),updateMenu).delete(protect,authorize('admin'),deleteMenu);
+router.route('/').get(getMenus).post(protect, authorize('admin'), addMenu);
+router.route('/:id').get(protect, getMenu).put(protect, authorize('admin'), updateMenu).delete(protect, authorize('admin'), deleteMenu);
 
 
-module.exports=router;
+module.exports = router;
